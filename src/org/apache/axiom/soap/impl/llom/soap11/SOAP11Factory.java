a06c6ffb59cd63c9280844d16781da5e94d556e1
Eran Chinthaka <chinthaka@apache.org>
2006-04-25 06:11:30 +0000

    Reverting changes as Axis2 is build is failing. (There should be a mail from me to the list from me explaining more on this. )

--- src/org/apache/axiom/soap/impl/llom/soap11/SOAP11Factory.java
+++ src/org/apache/axiom/soap/impl/llom/soap11/SOAP11Factory.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KINgetD, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -272,8 +272,6 @@ public class SOAP11Factory extends OMLinkedListImplFactory implements SOAPFactor
         SOAPEnvelopeImpl env = new SOAPEnvelopeImpl(ns, this);
         createSOAPHeader(env);
         createSOAPBody(env);
-        SOAPMessageImpl msg = new SOAPMessageImpl();
-        msg.setSOAPEnvelope(env);
         return env;
     }
 
