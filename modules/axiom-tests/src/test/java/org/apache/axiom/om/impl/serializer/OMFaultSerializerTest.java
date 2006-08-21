e2676286a4e074ec9cf5cb50d2cc09d87344ca00
Eran Chinthaka <chinthaka@apache.org>
2006-08-21 07:06:58 +0000

    This test was just printing the output to the console. Removed that by just serializing. Rich or some one wanna actually test something indide this test, later?

--- modules/axiom-tests/src/test/java/org/apache/axiom/om/impl/serializer/OMFaultSerializerTest.java
+++ modules/axiom-tests/src/test/java/org/apache/axiom/om/impl/serializer/OMFaultSerializerTest.java
@@ -17,21 +17,11 @@
 package org.apache.axiom.om.impl.serializer;
 
 import org.apache.axiom.om.AbstractTestCase;
-import org.apache.axiom.om.OMAbstractFactory;
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMXMLParserWrapper;
-import org.apache.axiom.om.impl.llom.factory.OMXMLBuilderFactory;
-import org.apache.axiom.om.impl.serialize.StreamingOMSerializer;
-import org.apache.axiom.soap.SOAPBody;
-import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axiom.soap.impl.builder.StAXSOAPModelBuilder;
 
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.XMLStreamWriter;
-import java.io.File;
 import java.io.FileReader;
 
 public class OMFaultSerializerTest extends AbstractTestCase {
@@ -53,18 +43,20 @@ public class OMFaultSerializerTest extends AbstractTestCase {
                     createXMLStreamReader(
                             new FileReader(
                                     getTestResourceFile("soap/soap11/soapfault2.xml")));
-       
+
     }
 
     /**
      * Test SOAPFault that does not disable the default namespace
      * (i.e. does not use xmlns="")
+     *
      * @throws Exception
      */
     public void test1() throws Exception {
     	StAXSOAPModelBuilder builder = new StAXSOAPModelBuilder(reader1, null);
         OMElement ome = builder.getDocumentElement();
-        System.out.println(ome);
+        ome.toString();
+//        System.out.println(ome);
     }
     /**
      * Test SOAPFault that does disable the default namespace
@@ -74,7 +66,8 @@ public class OMFaultSerializerTest extends AbstractTestCase {
     public void test2() throws Exception {
     	StAXSOAPModelBuilder builder = new StAXSOAPModelBuilder(reader2, null);
         OMElement ome = builder.getDocumentElement();
-        System.out.println(ome);
+        ome.toString();
+//        System.out.println(ome);
     }
 }
     
