ad78331ee4091f2dbd4be8c5be279115eb631e6f
Andreas Veithen <veithen@apache.org>
2008-12-22 21:37:10 +0000

    DOOM: Fixed an issue in ParentNode#getChildren that caused this method to return an empty collection if the first node had not been build yet.

--- modules/axiom-dom/src/main/java/org/apache/axiom/om/impl/dom/ParentNode.java
+++ modules/axiom-dom/src/main/java/org/apache/axiom/om/impl/dom/ParentNode.java
@@ -83,7 +83,7 @@ public abstract class ParentNode extends ChildNode implements OMContainerEx {
     }
 
     public Iterator getChildren() {
-        return new OMChildrenIterator(this.firstChild);
+        return new OMChildrenIterator(getFirstOMChild());
     }
 
     /**
