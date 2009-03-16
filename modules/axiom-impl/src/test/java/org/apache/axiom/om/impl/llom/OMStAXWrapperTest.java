2f56b4adf1c9163f860875bfeff318fde67d3abf
Andreas Veithen <veithen@apache.org>
2009-03-16 08:30:46 +0000

    Added test for WSCOMMONS-453.

--- modules/axiom-impl/src/test/java/org/apache/axiom/om/impl/llom/OMStAXWrapperTest.java
+++ modules/axiom-impl/src/test/java/org/apache/axiom/om/impl/llom/OMStAXWrapperTest.java
@@ -18,11 +18,39 @@
  */
 package org.apache.axiom.om.impl.llom;
 
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.axiom.om.OMDataSource;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMSourcedElement;
+import org.apache.axiom.om.ds.CharArrayDataSource;
 import org.apache.axiom.om.impl.OMStAXWrapperTestBase;
 import org.apache.axiom.om.impl.llom.factory.OMLinkedListMetaFactory;
 
 public class OMStAXWrapperTest extends OMStAXWrapperTestBase {
     public OMStAXWrapperTest() {
         super(new OMLinkedListMetaFactory());
+    }
+    
+    // Test for WSCOMMONS-453
+    public void _testOMSourcedElementDescendent() throws Exception {
+        OMFactory omFactory = omMetaFactory.getOMFactory();
+        OMDataSource ds = new CharArrayDataSource("<a>test</a>".toCharArray());
+        OMElement root = omFactory.createOMElement(new QName("root"));
+        OMSourcedElement child = omFactory.createOMElement(ds, "a", null);
+        root.addChild(child);
+        assertFalse(child.isExpanded());
+        XMLStreamReader stream = root.getXMLStreamReader();
+        assertEquals(XMLStreamReader.START_ELEMENT, stream.next());
+        assertEquals("root", stream.getLocalName());
+        assertEquals(XMLStreamReader.START_ELEMENT, stream.next());
+        assertEquals(XMLStreamReader.CHARACTERS, stream.next());
+        assertEquals("test", stream.getText());
+        assertEquals(XMLStreamReader.END_ELEMENT, stream.next());
+        assertEquals(XMLStreamReader.END_ELEMENT, stream.next());
+        assertEquals(XMLStreamReader.END_DOCUMENT, stream.next());
+        assertFalse(child.isExpanded());
     }
 }
