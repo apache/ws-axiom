6518da2ed37455497a49c536ea9f79b3056d6902
Andreas Veithen <veithen@apache.org>
2009-05-21 21:35:04 +0000

    Moved the unit tests for IncomingAttachment(Streams|InputStream) to the right module.

--- modules/axiom-api/src/test/java/org/apache/axiom/attachments/IncomingAttachmentTest.java
+++ modules/axiom-api/src/test/java/org/apache/axiom/attachments/IncomingAttachmentTest.java
@@ -27,12 +27,7 @@ import java.io.InputStream;
 
 /** Test the PartOnFile class */
 
-public class IncomingAttachmentInputTest extends AbstractTestCase {
-
-    public IncomingAttachmentInputTest(String testName) {
-        super(testName);
-    }
-
+public class IncomingAttachmentTest extends AbstractTestCase {
     String inMimeFileName = "mtom/MTOMAttachmentStream.bin";
     String contentTypeString =
             "multipart/related; boundary=\"MIMEBoundaryurn:uuid:A3ADBAEE51A1A87B2A11443668160701\"; type=\"application/xop+xml\"; start=\"<0.urn:uuid:A3ADBAEE51A1A87B2A11443668160702@apache.org>\"; start-info=\"application/soap+xml\"; charset=UTF-8;action=\"mtomSample\"";
