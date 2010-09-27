73599fdb4b493dda3259ff5e8c647ac292d1f665
Andreas Veithen <veithen@apache.org>
2010-09-27 22:03:00 +0000

    Moved the character encoding autodetection code (Appendix F.1 of the XML specs) into a separate class so that we can reuse if for other broken StAX implementations.

--- modules/axiom-api/src/main/java/org/apache/axiom/util/stax/dialect/BEAInputFactoryWrapper.java
+++ modules/axiom-api/src/main/java/org/apache/axiom/util/stax/dialect/BEAInputFactoryWrapper.java
@@ -19,9 +19,7 @@
 
 package org.apache.axiom.util.stax.dialect;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PushbackInputStream;
 import java.io.Reader;
 
 import javax.xml.stream.XMLInputFactory;
@@ -50,62 +48,9 @@ class BEAInputFactoryWrapper extends XMLInputFactoryWrapper {
         // information is already available from the stream reader, so that we don't need to
         // reimplement this part.
         // TODO: this needs some more unit testing!
-        byte[] startBytes = new byte[4];
-        try {
-            boolean useMark = stream.markSupported();
-            if (useMark) {
-                stream.mark(4);
-            } else {
-                stream = new PushbackInputStream(stream, 4);
-            }
-            int read = 0;
-            do {
-                int c = stream.read(startBytes, read, 4-read);
-                if (c == -1) {
-                    throw new XMLStreamException("Unexpected end of stream");
-                }
-                read += c;
-            } while (read < 4);
-            if (useMark) {
-                stream.reset();
-            } else {
-                ((PushbackInputStream)stream).unread(startBytes);
-            }
-        } catch (IOException ex) {
-            throw new XMLStreamException("Unable to read start bytes", ex);
-        }
-        int marker = ((startBytes[0] & 0xFF) << 24) + ((startBytes[1] & 0xFF) << 16)
-                + ((startBytes[2] & 0xFF) << 8) + (startBytes[3] & 0xFF);
-        String encoding;
-        switch (marker) {
-            case 0x0000FEFF:
-            case 0xFFFE0000:
-            case 0x0000FFFE:
-            case 0xFEFF0000:
-            case 0x0000003C:
-            case 0x3C000000:
-            case 0x00003C00:
-            case 0x003C0000:
-                encoding = "UCS-4";
-                break;
-            case 0x003C003F:
-                encoding = "UTF-16BE";
-                break;
-            case 0x3C003F00:
-                encoding = "UTF-16LE";
-                break;
-            case 0x3C3F786D:
-                encoding = "UTF-8";
-                break;
-            default:
-                if ((marker & 0xFFFF0000) == 0xFEFF0000) {
-                    encoding = "UTF-16BE";
-                } else if ((marker & 0xFFFF0000) == 0xFFFE0000) {
-                    encoding = "UTF-16LE";
-                } else {
-                    encoding = "UTF-8";
-                }
-        }
+        EncodingDetectionHelper helper = new EncodingDetectionHelper(stream);
+        stream = helper.getInputStream();
+        String encoding = helper.detectEncoding();
         XMLStreamReader reader;
         if (systemId == null) {
             reader = super.createXMLStreamReader(stream);
