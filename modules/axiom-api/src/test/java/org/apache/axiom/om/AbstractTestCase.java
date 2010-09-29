c21e27d740f827a3e351c67a480d1b60ecd7506f
Andreas Veithen <veithen@apache.org>
2010-09-29 21:39:23 +0000

    Use class loader instead of FileInputStream (revert of r817821).

--- modules/axiom-api/src/test/java/org/apache/axiom/om/AbstractTestCase.java
+++ modules/axiom-api/src/test/java/org/apache/axiom/om/AbstractTestCase.java
@@ -21,7 +21,6 @@ package org.apache.axiom.om;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -104,9 +103,9 @@ public abstract class AbstractTestCase
     }
     
     public static String[] getConformanceTestFiles() throws Exception {
-        InputStream is = new FileInputStream("../axiom-api/target/generated-test-resources/conformance/filelist");
-        BufferedReader in = new BufferedReader(new InputStreamReader(is));
-        
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                AbstractTestCase.class.getClassLoader().getResourceAsStream(
+                        "conformance/filelist")));
         String line;
         List result = new ArrayList(10);
         while ((line = in.readLine()) != null) {
