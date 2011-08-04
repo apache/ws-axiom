0959423fa1e5b5b858ef813ebdac25c7c875e5cc
Andreas Veithen <veithen@apache.org>
2011-08-04 20:52:10 +0000

    Moved testCachedFilesExpired to a separate test class. It is not testing anything in Attachments.

--- modules/axiom-api/src/test/java/org/apache/axiom/attachments/AttachmentsTest.java
+++ modules/axiom-api/src/test/java/org/apache/axiom/attachments/AttachmentsTest.java
@@ -20,7 +20,6 @@ package org.apache.axiom.attachments;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -443,76 +442,5 @@ public class AttachmentsTest extends AbstractTestCase {
 
     public void testReadBase64EncodedAttachmentWithPartOnFile() throws Exception {
         testReadBase64EncodedAttachment(true);
-    }
-
-    public void testCachedFilesExpired() throws Exception {
-        
-        // Set file expiration to 10 seconds
-        long INTERVAL = 3 * 1000; // 3 seconds for Thread to sleep
-
-       
-        // Get the AttachmentCacheMonitor and force it to remove files after
-        // 10 seconds.
-        AttachmentCacheMonitor acm = AttachmentCacheMonitor.getAttachmentCacheMonitor();
-        int previousTime = acm.getTimeout();
-        
-        try {
-            acm.setTimeout(10); 
-
-
-            File aFile = new File("A");
-            aFile.createNewFile();
-            String aFileName = aFile.getCanonicalPath();
-            acm.register(aFileName);
-
-            Thread.sleep(INTERVAL);
-
-            File bFile = new File("B");
-            bFile.createNewFile();
-            String bFileName = bFile.getCanonicalPath();
-            acm.register(bFileName);
-
-            Thread.sleep(INTERVAL);
-
-            acm.access(aFileName);
-
-            // time since file A registration <= cached file expiration
-            assertTrue("File A should still exist", aFile.exists());
-
-            Thread.sleep(INTERVAL);
-
-            acm.access(bFileName);
-
-            // time since file B registration <= cached file expiration
-            assertTrue("File B should still exist", bFile.exists());
-
-            Thread.sleep(INTERVAL);
-
-            File cFile = new File("C");
-            cFile.createNewFile();
-            String cFileName = cFile.getCanonicalPath();
-            acm.register(cFileName);
-            acm.access(bFileName);
-
-            Thread.sleep(INTERVAL);
-
-            acm.checkForAgedFiles();
-
-            // time since file C registration <= cached file expiration
-            assertTrue("File C should still exist", cFile.exists());
-
-            Thread.sleep(10* INTERVAL);  // Give task loop time to delete aged files
-
-
-            // All files should be gone by now
-            assertFalse("File A should no longer exist", aFile.exists());
-            assertFalse("File B should no longer exist", bFile.exists());
-            assertFalse("File C should no longer exist", cFile.exists());
-        } finally {
-       
-            // Reset the timeout to the previous value so that no 
-            // other tests are affected
-            acm.setTimeout(previousTime);
-        }
     }
 }
