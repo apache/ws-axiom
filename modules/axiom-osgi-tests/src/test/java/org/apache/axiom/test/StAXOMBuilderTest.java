6be63826227e2219bf538b144685ac19e820588f
Andreas Veithen <veithen@apache.org>
2011-10-08 17:01:53 +0000

    Merged latest changes from trunk.

--- modules/axiom-osgi-tests/src/test/java/org/apache/axiom/test/StAXOMBuilderTest.java
+++ modules/axiom-osgi-tests/src/test/java/org/apache/axiom/test/StAXOMBuilderTest.java
@@ -18,21 +18,27 @@
  */
 package org.apache.axiom.test;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayInputStream;
 
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.om.impl.builder.StAXOMBuilder;
-import org.apache.felix.ipojo.junit4osgi.OSGiTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
 
-public class StAXOMBuilderTest extends OSGiTestCase {
-	
-	private String xmlString = "<a:testElement xmlns:a=\"http://test/namespace\" />";
-	
-	public void testLLOMOMFactoryServicePresent() throws Exception {
-		ByteArrayInputStream bais = new ByteArrayInputStream(xmlString.getBytes());
-		StAXOMBuilder sb = new StAXOMBuilder(bais);
-		OMElement oe = sb.getDocumentElement();
-		assertEquals("testElement",oe.getLocalName());
-		assertEquals("http://test/namespace", oe.getNamespace().getNamespaceURI());
-	}
+@RunWith(MavenConfiguredJUnit4TestRunner.class)
+public class StAXOMBuilderTest {
+    
+    private String xmlString = "<a:testElement xmlns:a=\"http://test/namespace\" />";
+    
+    @Test
+    public void testLLOMOMFactoryServicePresent() throws Exception {
+        ByteArrayInputStream bais = new ByteArrayInputStream(xmlString.getBytes());
+        StAXOMBuilder sb = new StAXOMBuilder(bais);
+        OMElement oe = sb.getDocumentElement();
+        assertEquals("testElement",oe.getLocalName());
+        assertEquals("http://test/namespace", oe.getNamespace().getNamespaceURI());
+    }
 }
