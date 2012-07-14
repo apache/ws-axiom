91fbfcc2d0ef0a434119dcfd7abd79658e3b00d5
Andreas Veithen <veithen@apache.org>
2012-07-14 11:14:00 +0000

    * Consistently use SOAPProcessingException to report violations of the SOAP specs. * Increased test coverage of StAXSOAPModelBuilder.

--- modules/axiom-api/src/main/java/org/apache/axiom/soap/impl/builder/StAXSOAPModelBuilder.java
+++ modules/axiom-api/src/main/java/org/apache/axiom/soap/impl/builder/StAXSOAPModelBuilder.java
@@ -376,12 +376,12 @@ public class StAXSOAPModelBuilder extends StAXOMBuilder implements SOAPModelBuil
 
     /** Method createDTD. Overriding the default behaviour as a SOAPMessage should not have a DTD. */
     protected OMNode createDTD() throws OMException {
-        throw new OMException("SOAP message MUST NOT contain a Document Type Declaration(DTD)");
+        throw new SOAPProcessingException("SOAP message MUST NOT contain a Document Type Declaration(DTD)");
     }
 
     /** Method createPI. Overriding the default behaviour as a SOAP Message should not have a PI. */
     protected OMNode createPI() throws OMException {
-        throw new OMException("SOAP message MUST NOT contain Processing Instructions(PI)");
+        throw new SOAPProcessingException("SOAP message MUST NOT contain Processing Instructions(PI)");
     }
 
     // Necessary to allow SOAPBuilderHelper to access this method
