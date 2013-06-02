4a940d1ff1aed1898dde5303d4126ec4a149c09f
Andreas Veithen <veithen@apache.org>
2013-06-02 19:06:12 +0000

    Javadoc cleanup.

--- modules/axiom-api/src/main/java/org/apache/axiom/om/OMText.java
+++ modules/axiom-api/src/main/java/org/apache/axiom/om/OMText.java
@@ -67,10 +67,8 @@ public interface OMText extends OMNode {
     QName getTextAsQName();
 
     /**
-     * Returns the Namespace if this contains a QName Return null otherwise
-     *
-     * @deprecated This API is going away.  Please use getTextAsQName() instead.
-     * @return OMNamespace
+     * @deprecated This API is going away. Please use {@link OMElement#getTextAsQName()} instead.
+     * @see {@link #getTextAsQName()}
      */
     OMNamespace getNamespace();
 
