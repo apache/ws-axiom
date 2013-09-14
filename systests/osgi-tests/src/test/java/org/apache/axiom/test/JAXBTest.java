666899f099b8617784cc0fef94a33556925d79d6
Andreas Veithen <veithen@apache.org>
2013-09-14 21:47:14 +0000

    Upgraded to latest Pax Exam version.

--- systests/osgi-tests/src/test/java/org/apache/axiom/test/JAXBTest.java
+++ systests/osgi-tests/src/test/java/org/apache/axiom/test/JAXBTest.java
@@ -18,6 +18,12 @@
  */
 package org.apache.axiom.test;
 
+import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.MavenUtils.asInProject;
+
 import java.io.ByteArrayOutputStream;
 
 import javax.xml.bind.JAXBContext;
@@ -28,13 +34,35 @@ import org.apache.axiom.om.OMSourcedElement;
 import org.apache.axiom.om.ds.jaxb.JAXBOMDataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 
 /**
  * Tests that the axiom-jaxb bundle can be loaded successfully and is operational.
  */
-@RunWith(MavenConfiguredJUnit4TestRunner.class)
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
 public class JAXBTest {
+    @Configuration
+    public static Option[] configuration() {
+        return options(
+                mavenBundle().groupId("org.apache.geronimo.specs").artifactId("geronimo-activation_1.1_spec").version(asInProject()),
+                mavenBundle().groupId("org.apache.servicemix.specs").artifactId("org.apache.servicemix.specs.stax-api-1.0").version(asInProject()),
+                mavenBundle().groupId("org.codehaus.woodstox").artifactId("stax2-api").version(asInProject()),
+                mavenBundle().groupId("org.codehaus.woodstox").artifactId("woodstox-core-asl").version(asInProject()),
+                mavenBundle().groupId("org.apache.james").artifactId("apache-mime4j-core").version(asInProject()),
+                mavenBundle().groupId("org.apache.ws.commons.axiom").artifactId("axiom-api").version(asInProject()),
+                mavenBundle().groupId("org.apache.ws.commons.axiom").artifactId("axiom-impl").version(asInProject()),
+                mavenBundle().groupId("org.apache.servicemix.specs").artifactId("org.apache.servicemix.specs.jaxb-api-2.1").version(asInProject()),
+                mavenBundle().groupId("org.apache.servicemix.bundles").artifactId("org.apache.servicemix.bundles.jaxb-impl").version(asInProject()),
+                mavenBundle().groupId("org.apache.ws.commons.axiom").artifactId("axiom-jaxb").version(asInProject()),
+                junitBundles(),
+                frameworkProperty("foo").value("bar"));
+    }
+    
     @Test
     public void test() throws Exception {
         OMFactory factory = OMAbstractFactory.getOMFactory();
--- systests/osgi-tests/src/test/java/org/apache/axiom/test/OMAbstractFactoryTest.java
+++ systests/osgi-tests/src/test/java/org/apache/axiom/test/OMAbstractFactoryTest.java
@@ -19,14 +19,38 @@
 package org.apache.axiom.test;
 
 import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.frameworkProperty;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.MavenUtils.asInProject;
 
 import org.apache.axiom.om.OMAbstractFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
 
-@RunWith(MavenConfiguredJUnit4TestRunner.class)
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
 public class OMAbstractFactoryTest {
+    @Configuration
+    public static Option[] configuration() {
+        return options(
+                mavenBundle().groupId("org.apache.geronimo.specs").artifactId("geronimo-activation_1.1_spec").version(asInProject()),
+                mavenBundle().groupId("org.apache.servicemix.specs").artifactId("org.apache.servicemix.specs.stax-api-1.0").version(asInProject()),
+                mavenBundle().groupId("org.codehaus.woodstox").artifactId("stax2-api").version(asInProject()),
+                mavenBundle().groupId("org.codehaus.woodstox").artifactId("woodstox-core-asl").version(asInProject()),
+                mavenBundle().groupId("org.apache.james").artifactId("apache-mime4j-core").version(asInProject()),
+                mavenBundle().groupId("org.apache.ws.commons.axiom").artifactId("axiom-api").version(asInProject()),
+                mavenBundle().groupId("org.apache.ws.commons.axiom").artifactId("axiom-impl").version(asInProject()),
+                junitBundles(),
+                frameworkProperty("foo").value("bar"));
+    }
+    
     @Test
     public void testgetOMFactory() throws Exception {
         assertNotNull(OMAbstractFactory.getOMFactory());
