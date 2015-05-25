9c2eb94610c3b71ae057295c559da9f93af39298
Andreas Veithen <veithen@apache.org>
2015-05-25 03:09:14 +0000

    Replace tabs with spaces.

--- etc/dist.py
+++ etc/dist.py
@@ -26,7 +26,7 @@ release = argv[1]
 mkdir(release)
 
 for classifier in [ "bin", "source-release" ]:
-	for suffix in [ "zip", "zip.asc", "zip.md5"]:
-		file = "axiom-" + release + "-" + classifier + "." + suffix
-		urlretrieve("http://repository.apache.org/content/repositories/releases/org/apache/ws/commons/axiom/axiom/" + release + "/" + file, join(release, file))
+    for suffix in [ "zip", "zip.asc", "zip.md5"]:
+        file = "axiom-" + release + "-" + classifier + "." + suffix
+        urlretrieve("http://repository.apache.org/content/repositories/releases/org/apache/ws/commons/axiom/axiom/" + release + "/" + file, join(release, file))
 
