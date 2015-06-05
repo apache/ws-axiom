4a6082c8129a56ae8f9bbfd579fe7c074e295e49
Andreas Veithen <veithen@apache.org>
2015-06-05 07:01:59 +0000

    Remove unused imports.

--- implementations/axiom-dom/src/main/java/org/apache/axiom/om/impl/dom/NodeImpl.java
+++ implementations/axiom-dom/src/main/java/org/apache/axiom/om/impl/dom/NodeImpl.java
@@ -38,12 +38,7 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.UserDataHandler;
 
-import java.io.OutputStream;
-import java.io.Writer;
 import java.util.Hashtable;
-
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 
 public abstract class NodeImpl extends InformationItem implements DOMNode {
 
