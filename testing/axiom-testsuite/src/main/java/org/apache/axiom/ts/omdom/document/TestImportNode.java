f045949c15e3ba9a52c863173cdb972e81b087b4
Andreas Veithen <veithen@apache.org>
2015-11-07 22:24:22 +0000

    Change TestImportNode to use xml-truth.

--- testing/axiom-testsuite/src/main/java/org/apache/axiom/ts/omdom/document/TestImportNode.java
+++ testing/axiom-testsuite/src/main/java/org/apache/axiom/ts/omdom/document/TestImportNode.java
@@ -19,24 +19,19 @@
 
 package org.apache.axiom.ts.omdom.document;
 
-import java.util.Iterator;
-
-import org.apache.axiom.om.AbstractTestCase;
-import org.apache.axiom.om.OMAttribute;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMException;
-import org.apache.axiom.om.OMMetaFactory;
-import org.apache.axiom.om.OMNode;
-import org.apache.axiom.om.dom.DOMMetaFactory;
-import org.apache.axiom.ts.AxiomTestCase;
-import org.w3c.dom.Attr;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import static com.google.common.truth.Truth.assertAbout;
+import static org.apache.axiom.truth.xml.XMLTruth.xml;
 
 import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.axiom.om.AbstractTestCase;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMMetaFactory;
+import org.apache.axiom.om.dom.DOMMetaFactory;
+import org.apache.axiom.ts.AxiomTestCase;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 public class TestImportNode extends AxiomTestCase {
     public TestImportNode(OMMetaFactory metaFactory) {
@@ -50,59 +45,8 @@ public class TestImportNode extends AxiomTestCase {
                 AbstractTestCase.getTestResource("sigEncr.xml"));
         Document doc2 = ((DOMMetaFactory)metaFactory).newDocumentBuilderFactory().newDocumentBuilder().newDocument();
         Node n = doc2.importNode(doc.getDocumentElement(), true);
-        compare(doc.getDocumentElement(), (OMElement) n);
-    }
-
-    private static void compare(Element ele, OMElement omele) throws Exception {
-        if (ele == null && omele == null) {
-            return;
-        } else if (ele != null && omele != null) {
-            assertEquals("Element name not correct",
-                                  ele.getLocalName(),
-                                  omele.getLocalName());
-            if (omele.getNamespace() != null) {
-                assertEquals("Namespace URI not correct",
-                             ele.getNamespaceURI(),
-                             omele.getNamespace().getNamespaceURI());
-
-            }
-
-            //go through the attributes
-            NamedNodeMap map = ele.getAttributes();
-            Iterator attIterator = omele.getAllAttributes();
-            OMAttribute omattribute;
-            while (attIterator.hasNext()) {
-                omattribute = (OMAttribute) attIterator.next();
-                Node node = map.getNamedItemNS(
-                        omattribute.getNamespaceURI(),
-                        omattribute.getLocalName());
-                if (node.getNodeType() == Node.ATTRIBUTE_NODE) {
-                    Attr attr = (Attr) node;
-                    assertEquals(attr.getValue(),
-                                 omattribute.getAttributeValue());
-                } else {
-                    throw new OMException("return type is not a Attribute");
-                }
-
-            }
-            Iterator it = omele.getChildren();
-            NodeList list = ele.getChildNodes();
-            for (int i = 0; i < list.getLength(); i++) {
-                Node node = list.item(i);
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    assertTrue(it.hasNext());
-                    OMNode tempOmNode = (OMNode) it.next();
-                    while (tempOmNode.getType() != OMNode.ELEMENT_NODE) {
-                        assertTrue(it.hasNext());
-                        tempOmNode = (OMNode) it.next();
-                    }
-                    compare((Element) node, (OMElement) tempOmNode);
-                }
-            }
-
-
-        } else {
-            throw new Exception("One is null");
-        }
+        assertAbout(xml())
+                .that(xml(OMElement.class, (OMElement)n))
+                .hasSameContentAs(xml(Element.class, doc.getDocumentElement()));
     }
 }
