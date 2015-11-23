493790a3285c1a07d072ef7a957dc3856d8d7384
Andreas Veithen <veithen@apache.org>
2015-11-23 23:26:55 +0000

    Unify the (get|set)(Value|SubCode) code in the SOAPFaultClassifier implementations.

--- implementations/axiom-dom/src/main/java/org/apache/axiom/soap/impl/dom/soap12/SOAP12FaultSubCodeImpl.java
+++ implementations/axiom-dom/src/main/java/org/apache/axiom/soap/impl/dom/soap12/SOAP12FaultSubCodeImpl.java
@@ -20,18 +20,11 @@
 package org.apache.axiom.soap.impl.dom.soap12;
 
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.util.ElementHelper;
-import org.apache.axiom.soap.SOAP12Constants;
-import org.apache.axiom.soap.SOAPFaultSubCode;
-import org.apache.axiom.soap.SOAPFaultValue;
 import org.apache.axiom.soap.SOAPProcessingException;
 import org.apache.axiom.soap.impl.dom.SOAPElement;
 import org.apache.axiom.soap.impl.intf.AxiomSOAP12FaultSubCode;
 
 public class SOAP12FaultSubCodeImpl extends SOAPElement implements AxiomSOAP12FaultSubCode {
-    private SOAPFaultValue value;
-    private SOAPFaultSubCode subCode;
-    
     public void checkParent(OMElement parent) throws SOAPProcessingException {
         if (!((parent instanceof SOAP12FaultSubCodeImpl) ||
                 (parent instanceof SOAP12FaultCodeImpl))) {
@@ -40,38 +33,5 @@ public class SOAP12FaultSubCodeImpl extends SOAPElement implements AxiomSOAP12Fa
                             "or SOAP FaultCodeValue as the parent. But received some " +
                             "other implementation");
         }
-    }
-
-    public void setSubCode(SOAPFaultSubCode subCode) throws SOAPProcessingException {
-        if (!(subCode instanceof SOAP12FaultSubCodeImpl)) {
-            throw new SOAPProcessingException(
-                    "Expecting SOAP 1.2 implementation of SOAP Fault " +
-                            "Sub Code. But received some other implementation");
-        }
-        ElementHelper.setNewElement(this, this.subCode, subCode);
-    }
-
-    public SOAPFaultSubCode getSubCode() {
-        if (subCode == null) {
-            subCode = (SOAPFaultSubCode)getFirstChildWithName(SOAP12Constants.QNAME_FAULT_SUBCODE);
-        }
-        return subCode;
-    }
-
-    public void setValue(SOAPFaultValue soapFaultSubCodeValue)
-            throws SOAPProcessingException {
-        if (!(soapFaultSubCodeValue instanceof SOAP12FaultValueImpl)) {
-            throw new SOAPProcessingException(
-                    "Expecting SOAP 1.2 implementation of SOAP Fault Value. " +
-                            "But received some other implementation");
-        }
-        ElementHelper.setNewElement(this, value, soapFaultSubCodeValue);
-    }
-
-    public SOAPFaultValue getValue() {
-        if (value == null) {
-            value = (SOAPFaultValue)getFirstChildWithName(SOAP12Constants.QNAME_FAULT_VALUE);
-        }
-        return value;
     }
 }
