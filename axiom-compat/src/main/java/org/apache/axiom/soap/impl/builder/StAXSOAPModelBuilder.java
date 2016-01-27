fa8be329c33a3cc54a35d3fa548fa660ff207e15
Andreas Veithen <veithen@apache.org>
2016-01-27 23:44:09 +0000

    Restore enough binary compatibility with Axiom 1.2.x to allow us to execute the Spring-WS tests.

---
+++ axiom-compat/src/main/java/org/apache/axiom/soap/impl/builder/StAXSOAPModelBuilder.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.axiom.soap.impl.builder;
+
+import javax.xml.stream.XMLStreamReader;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMDocument;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMException;
+import org.apache.axiom.om.OMXMLBuilderFactory;
+import org.apache.axiom.soap.SOAPEnvelope;
+import org.apache.axiom.soap.SOAPFactory;
+import org.apache.axiom.soap.SOAPMessage;
+import org.apache.axiom.soap.SOAPModelBuilder;
+
+/**
+ * @deprecated Please use the {@link OMXMLBuilderFactory} API to create builders.
+ */
+public class StAXSOAPModelBuilder implements SOAPModelBuilder {
+    private final SOAPModelBuilder target;
+
+    protected StAXSOAPModelBuilder(SOAPModelBuilder target) {
+        this.target = target;
+    }
+    
+    public StAXSOAPModelBuilder(XMLStreamReader parser, SOAPFactory factory, String soapVersion) {
+        this(OMXMLBuilderFactory.createStAXSOAPModelBuilder(factory.getMetaFactory(), parser));
+        // TODO: check SOAP version
+    }
+    
+    public StAXSOAPModelBuilder(XMLStreamReader parser, String soapVersion) {
+        this(OMXMLBuilderFactory.createStAXSOAPModelBuilder(OMAbstractFactory.getMetaFactory(), parser));
+        // TODO: check SOAP version
+    }
+    
+    public SOAPEnvelope getSOAPEnvelope() {
+        return target.getSOAPEnvelope();
+    }
+
+    public int next() throws OMException {
+        return target.next();
+    }
+
+    public SOAPMessage getSOAPMessage() {
+        return target.getSOAPMessage();
+    }
+
+    public void discard(OMElement el) throws OMException {
+        target.discard(el);
+    }
+
+    public void setCache(boolean b) throws OMException {
+        target.setCache(b);
+    }
+
+    public boolean isCache() {
+        return target.isCache();
+    }
+
+    public Object getParser() {
+        return target.getParser();
+    }
+
+    public boolean isCompleted() {
+        return target.isCompleted();
+    }
+
+    public OMDocument getDocument() {
+        return target.getDocument();
+    }
+
+    public OMElement getDocumentElement() {
+        return target.getDocumentElement();
+    }
+
+    public OMElement getDocumentElement(boolean discardDocument) {
+        return target.getDocumentElement(discardDocument);
+    }
+
+    public String getCharacterEncoding() {
+        return target.getCharacterEncoding();
+    }
+
+    public void close() {
+        target.close();
+    }
+
+    public void detach() {
+        target.detach();
+    }
+}
