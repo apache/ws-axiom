04da0ae664201f9a95b83d57e23ef447cb9cfdaa
Andreas Veithen <veithen@apache.org>
2016-02-21 11:34:56 +0000

    Increase test coverage.

---
+++ testing/axiom-testsuite/src/main/java/org/apache/axiom/ts/om/comment/TestSerialize.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.axiom.ts.om.comment;
+
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.axiom.om.OMComment;
+import org.apache.axiom.om.OMMetaFactory;
+import org.apache.axiom.ts.AxiomTestCase;
+
+public class TestSerialize extends AxiomTestCase {
+    public TestSerialize(OMMetaFactory metaFactory) {
+        super(metaFactory);
+    }
+
+    @Override
+    protected void runTest() throws Throwable {
+        OMComment comment = metaFactory.getOMFactory().createOMComment(null, "test");
+        XMLStreamWriter writer = mock(XMLStreamWriter.class);
+        comment.serialize(writer);
+        verify(writer).writeComment(comment.getValue());
+        verify(writer, atMost(1)).flush();
+        verifyNoMoreInteractions(writer);
+    }
+}
