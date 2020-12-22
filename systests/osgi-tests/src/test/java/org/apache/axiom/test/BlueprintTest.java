55e90e363d4bfb2f7243382b2b59ec7b6e58c1fe
Andreas Veithen <veithen@apache.org>
2020-12-22 16:30:34 +0000

    Update Pax Exam

--- systests/osgi-tests/src/test/java/org/apache/axiom/test/BlueprintTest.java
+++ systests/osgi-tests/src/test/java/org/apache/axiom/test/BlueprintTest.java
@@ -41,6 +41,7 @@ public class BlueprintTest {
         return options(
                 url("link:classpath:org.apache.aries.proxy.api.link"),
                 url("link:classpath:org.apache.aries.proxy.link"),
+                url("link:classpath:org.apache.aries.blueprint.api.link"),
                 url("link:classpath:org.apache.aries.blueprint.core.link"),
                 url("link:classpath:com.sun.activation.javax.activation.link"),
                 url("link:classpath:org.apache.james.apache-mime4j-core.link"),
