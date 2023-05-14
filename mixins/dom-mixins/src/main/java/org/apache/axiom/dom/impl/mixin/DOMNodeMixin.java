eddfc8130f2c64e81fc205287229e67fdad1b701
Andreas Veithen <andreas.veithen@gmail.com>
2023-05-14 13:26:59 +0100

    Remove obsolete/redundant Javadoc

--- mixins/dom-mixins/src/main/java/org/apache/axiom/dom/impl/mixin/DOMNodeMixin.java
+++ mixins/dom-mixins/src/main/java/org/apache/axiom/dom/impl/mixin/DOMNodeMixin.java
@@ -140,42 +140,6 @@ public abstract class DOMNodeMixin implements DOMNode {
         return this == node;
     }
 
-    /**
-     * Tests whether two nodes are equal. <br>This method tests for equality of nodes, not sameness
-     * (i.e., whether the two nodes are references to the same object) which can be tested with
-     * <code>Node.isSameNode()</code>. All nodes that are the same will also be equal, though the
-     * reverse may not be true. <br>Two nodes are equal if and only if the following conditions are
-     * satisfied: <ul> <li>The two nodes are of the same type. </li> <li>The following string
-     * attributes are equal: <code>nodeName</code>, <code>localName</code>,
-     * <code>namespaceURI</code>, <code>prefix</code>, <code>nodeValue</code> . This is: they are
-     * both <code>null</code>, or they have the same length and are character for character
-     * identical. </li> <li>The <code>attributes</code> <code>NamedNodeMaps</code> are equal. This
-     * is: they are both <code>null</code>, or they have the same length and for each node that
-     * exists in one map there is a node that exists in the other map and is equal, although not
-     * necessarily at the same index. </li> <li>The <code>childNodes</code> <code>NodeLists</code>
-     * are equal. This is: they are both <code>null</code>, or they have the same length and contain
-     * equal nodes at the same index. Note that normalization can affect equality; to avoid this,
-     * nodes should be normalized before being compared. </li> </ul> <br>For two
-     * <code>DocumentType</code> nodes to be equal, the following conditions must also be satisfied:
-     * <ul> <li>The following string attributes are equal: <code>publicId</code>,
-     * <code>systemId</code>, <code>internalSubset</code>. </li> <li>The <code>entities</code>
-     * <code>NamedNodeMaps</code> are equal. </li> <li>The <code>notations</code>
-     * <code>NamedNodeMaps</code> are equal. </li> </ul> <br>On the other hand, the following do not
-     * affect equality: the <code>ownerDocument</code>, <code>baseURI</code>, and
-     * <code>parentNode</code> attributes, the <code>specified</code> attribute for
-     * <code>Attr</code> nodes, the <code>schemaTypeInfo</code> attribute for <code>Attr</code> and
-     * <code>Element</code> nodes, the <code>Text.isElementContentWhitespace</code> attribute for
-     * <code>Text</code> nodes, as well as any user data or event listeners registered on the nodes.
-     * <p ><b>Note:</b>  As a general rule, anything not mentioned in the description above is not
-     * significant in consideration of equality checking. Note that future versions of this
-     * specification may take into account more attributes and implementations conform to this
-     * specification are expected to be updated accordingly.
-     *
-     * @param node The node to compare equality with.
-     * @return Returns <code>true</code> if the nodes are equal, <code>false</code> otherwise.
-     * @since DOM Level 3
-     */
-
     //TODO : sumedha, complete
     @Override
     public boolean isEqualNode(Node node) {
@@ -324,21 +288,6 @@ public abstract class DOMNodeMixin implements DOMNode {
         throw new UnsupportedOperationException("TODO");
     }
 
-    /* *
-     * userData storage/hashtable will be called only when the user needs to set user data. Previously, it was done as,
-     * for every node a new Hashtable created making the excution very inefficient. According to profiles, no. of method
-     * invocations to setUserData() method is very low, so this implementation is better.
-     * Another option:
-     * TODO do a profile and check the times for hashtable initialization. If it's still higher, we have to go to second option
-     * Create a separate class(UserData) to store key and value pairs. Then put those objects to a array with a reasonable size.
-     * then grow it accordingly.  @ Kasun Gajasinghe 
-     * @param key userData key
-     * @param value userData value
-     * @param userDataHandler it seems all invocations sends null for this parameter.
-     *          Kept it for the moment just for being on the safe side.
-     * @return previous Object if one is set before.
-     */
-    
     @Override
     public Object setUserData(String key, Object value, UserDataHandler userDataHandler) {
         if (userData == null) {
