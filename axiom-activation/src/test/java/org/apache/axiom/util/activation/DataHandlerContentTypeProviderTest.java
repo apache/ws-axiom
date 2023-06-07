c2fd776e8af36906e30134709afee405f8167e36
Andreas Veithen <andreas.veithen@gmail.com>
2023-06-07 22:14:39 +0000

    Migrate axiom-activation to AssertJ

--- axiom-activation/src/test/java/org/apache/axiom/util/activation/DataHandlerContentTypeProviderTest.java
+++ axiom-activation/src/test/java/org/apache/axiom/util/activation/DataHandlerContentTypeProviderTest.java
@@ -18,7 +18,7 @@
  */
 package org.apache.axiom.util.activation;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.activation.DataHandler;
 
