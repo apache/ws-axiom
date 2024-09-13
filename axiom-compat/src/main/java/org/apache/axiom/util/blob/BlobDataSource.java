f940e3d775a7230d1eec1b78d8e45852fe4036c1
Andreas Veithen <andreas.veithen@gmail.com>
2024-09-13 17:13:29 +0000

    Fix Javadoc

--- axiom-compat/src/main/java/org/apache/axiom/util/blob/BlobDataSource.java
+++ axiom-compat/src/main/java/org/apache/axiom/util/blob/BlobDataSource.java
@@ -28,7 +28,8 @@ import org.apache.axiom.ext.activation.SizeAwareDataSource;
 /**
  * Data source backed by a {@link Blob}.
  * 
- * @deprecated Use {@link org.apache.axiom.blob.BlobDataSource} instead.
+ * @deprecated Use {@link org.apache.axiom.util.activation.DataHandlerUtils#toDataHandler(org.apache.axiom.blob.Blob)}
+ * and {@link jakarta.activation.DataHandler#getDataSource()} instead.
  */
 public class BlobDataSource implements SizeAwareDataSource {
     private final Blob blob;
