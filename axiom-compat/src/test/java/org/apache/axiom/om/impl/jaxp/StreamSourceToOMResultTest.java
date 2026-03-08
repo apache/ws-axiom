396e51cd5e7f7187a29ec3a48195522504b5aee1
Andreas Veithen-Knowles <andreas.veithen@gmail.com>
2026-03-08 23:11:03 +0000

    Migrate StreamSourceToOMResultTest to the new matrix test framework

--- axiom-compat/src/test/java/org/apache/axiom/om/impl/jaxp/StreamSourceToOMResultTest.java
+++ axiom-compat/src/test/java/org/apache/axiom/om/impl/jaxp/StreamSourceToOMResultTest.java
@@ -18,73 +18,35 @@
  */
 package org.apache.axiom.om.impl.jaxp;
 
-import static com.google.common.truth.Truth.assertAbout;
-import static org.apache.axiom.truth.xml.XMLTruth.xml;
+import java.util.stream.Stream;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.util.Iterator;
-
-import javax.xml.transform.stream.StreamSource;
-
-import junit.framework.TestSuite;
-
-import org.apache.axiom.om.OMAbstractFactory;
-import org.apache.axiom.om.OMMetaFactory;
 import org.apache.axiom.testing.multiton.Multiton;
-import org.apache.axiom.testutils.suite.MatrixTestCase;
-import org.apache.axiom.testutils.suite.MatrixTestSuiteBuilder;
-import org.apache.axiom.ts.jaxp.xslt.XSLTImplementation;
+import org.apache.axiom.testutils.suite.MatrixTest;
+import com.google.common.collect.ImmutableList;
+import org.apache.axiom.testutils.suite.MatrixTestFilters;
+import org.apache.axiom.testutils.suite.ParameterFanOutNode;
 import org.apache.axiom.ts.xml.XMLSample;
-import org.xml.sax.InputSource;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.TestFactory;
 
-@SuppressWarnings({"rawtypes", "deprecation"})
-public class StreamSourceToOMResultTest extends MatrixTestCase {
-    private static final String[] axiomImplementations = {"default", "dom"};
-
-    private final OMMetaFactory omMetaFactory;
-    private final XMLSample file;
-
-    private StreamSourceToOMResultTest(String axiomImplementation, XMLSample file) {
-        omMetaFactory = OMAbstractFactory.getMetaFactory(axiomImplementation);
-        this.file = file;
-        addTestParameter("axiomImplementation", axiomImplementation);
-        addTestParameter("file", file.getName());
-    }
-
-    @Override
-    protected void runTest() throws Throwable {
-        StreamSource source = new StreamSource(file.getUrl().toString());
-        OMResult result = new OMResult(omMetaFactory.getOMFactory());
-        XSLTImplementation.XALAN.newTransformerFactory().newTransformer().transform(source, result);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        result.getDocument().serialize(out);
-        InputSource actual = new InputSource();
-        actual.setByteStream(new ByteArrayInputStream(out.toByteArray()));
-        actual.setSystemId(file.getUrl().toString());
-        assertAbout(xml())
-                .that(actual)
-                .ignoringWhitespaceInPrologAndEpilog()
-                .expandingEntityReferences()
-                .hasSameContentAs(file.getUrl());
-    }
-
-    public static TestSuite suite() {
-        MatrixTestSuiteBuilder builder =
-                new MatrixTestSuiteBuilder() {
-                    @Override
-                    protected void addTests() {
-                        for (int i = 0; i < axiomImplementations.length; i++) {
-                            for (Iterator it = Multiton.getInstances(XMLSample.class).iterator();
-                                    it.hasNext(); ) {
-                                addTest(
-                                        new StreamSourceToOMResultTest(
-                                                axiomImplementations[i], (XMLSample) it.next()));
-                            }
-                        }
-                    }
-                };
-        builder.exclude("(|(file=sax-attribute-namespace-bug.xml)(file=large.xml))");
-        return builder.build();
+public class StreamSourceToOMResultTest {
+    @TestFactory
+    public Stream<DynamicNode> tests() {
+        MatrixTestFilters excludes =
+                MatrixTestFilters.builder()
+                        .add("(|(file=sax-attribute-namespace-bug.xml)(file=large.xml))")
+                        .build();
+        return new ParameterFanOutNode<>(
+                        String.class,
+                        ImmutableList.of("default", "dom"),
+                        "axiomImplementation",
+                        s -> s,
+                        new ParameterFanOutNode<>(
+                                XMLSample.class,
+                                Multiton.getInstances(XMLSample.class),
+                                "file",
+                                XMLSample::getName,
+                                new MatrixTest(StreamSourceToOMResultTestCase.class)))
+                .toDynamicNodes(excludes);
     }
 }
