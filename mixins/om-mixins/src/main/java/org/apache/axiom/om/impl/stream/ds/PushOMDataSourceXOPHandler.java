624ea8aa793227ad2e9ec24b0c9f5a7fb24a237c
Andreas Veithen-Knowles <andreas.veithen@gmail.com>
2026-06-11 06:36:02 +0000

    Fix Checker Framework errors in om-mixins

--- mixins/om-mixins/src/main/java/org/apache/axiom/om/impl/stream/ds/PushOMDataSourceXOPHandler.java
+++ mixins/om-mixins/src/main/java/org/apache/axiom/om/impl/stream/ds/PushOMDataSourceXOPHandler.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import org.apache.axiom.blob.Blob;
 import org.apache.axiom.core.stream.StreamException;
 import org.apache.axiom.core.stream.XmlHandler;
+import org.apache.axiom.core.stream.annotations.StringOrCharacterData;
 import org.apache.axiom.core.stream.xop.AbstractXOPDecodingFilterHandler;
 import org.apache.axiom.om.impl.intf.TextContent;
 import org.apache.axiom.om.impl.stream.xop.XOPHandler;
@@ -43,7 +44,7 @@ final class PushOMDataSourceXOPHandler extends AbstractXOPDecodingFilterHandler 
     }
 
     @Override
-    protected Object buildCharacterData(String contentID) throws StreamException {
+    protected @StringOrCharacterData Object buildCharacterData(String contentID) throws StreamException {
         Blob blob = blobs.get(contentID);
         if (blob == null) {
             throw new StreamException("No Blob found for content ID " + contentID);
